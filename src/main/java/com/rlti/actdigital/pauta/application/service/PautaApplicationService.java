858b89516a71ec2647453a2c48b266860be579a7
ranelho <ranelhol@gmail.com>
2023-05-16 14:25:09 -0300

    implementa valida horario de votacao

--- src/main/java/com/rlti/actdigital/pauta/application/service/PautaApplicationService.java
+++ src/main/java/com/rlti/actdigital/pauta/application/service/PautaApplicationService.java
@@ -11,9 +11,12 @@ import com.rlti.actdigital.pauta.domain.Pauta;
 import com.rlti.actdigital.pauta.domain.Votacao;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +27,9 @@ public class PautaApplicationService implements PautaService {
     private final PautaRepository pautaRepository;
     private final VotacaoRepository votacaoRepository;
     private final AssociadoRepository associadoRepository;
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+    LocalDateTime now = LocalDateTime.parse(formatter.format(LocalDateTime.now()));
 
     @Override
     public PautaResponse createPauta(PautaRequest request) {
@@ -42,15 +48,17 @@ public class PautaApplicationService implements PautaService {
 
     @Override
     public VotacaoResponse createVotacao(VotacaoRequest request) {
+
         log.info("[inicia] PautaApplicationService.createVotacao");
         Pauta pauta = pautaRepository.getById(request.getIdPauta());
         Associado associado = associadoRepository.findByCpf(request.getCpf());
         Optional<Votacao> jaVotou = votacaoRepository.findByAssociadoAndPauta(associado, pauta);
-        if(jaVotou.isPresent()) {
+        if (now.isAfter(pauta.getHorarioFim()))
+            throw APIException.build(HttpStatus.BAD_REQUEST, "Horário de votação encerrado");
+        else if (jaVotou.isPresent())
             throw APIException.build(HttpStatus.BAD_REQUEST, "Associado já votou nesta pauta");
-        }else if (associado.getStatus().equals(Status.UNABLE_TO_VOTE)) {
+        else if (associado.getStatus().equals(Status.UNABLE_TO_VOTE))
             throw APIException.build(HttpStatus.BAD_REQUEST, "Associado não pode votar nesta pauta");
-        }
         Votacao votacao = votacaoRepository.salva(new Votacao(request, associado, pauta));
         log.info("[finaliza] PautaApplicationService.createVotacao");
         return new VotacaoResponse(votacao);
