00d83e72e6f048177c5663904e91da6d63dfd858
ranelho <ranelhol@gmail.com>
2023-05-24 17:41:44 -0300

    corrige swagger http para default

--- src/main/java/com/rlti/actdigital/ActDigitalApplication.java
+++ src/main/java/com/rlti/actdigital/ActDigitalApplication.java
@@ -2,10 +2,12 @@ package com.rlti.actdigital;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info = @Info(title = "ACT Pautas", version = "1.0.0", description = "Gestão de Pautas"))
+@OpenAPIDefinition(info = @Info(title = "ACT Pautas", version = "1.0.0", description = "Gestão de Pautas"),
+		servers = {@Server(url = "/act/api", description = "Default Server URL")})
 @SpringBootApplication
 public class ActDigitalApplication {
 
