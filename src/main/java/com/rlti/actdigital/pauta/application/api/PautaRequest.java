f45f1ec39c52202fd3a733ea7e324a68b9480dc9
ranelho <ranelhol@gmail.com>
2023-07-15 09:59:04 -0300

    convert class PautaRequest em record

--- src/main/java/com/rlti/actdigital/pauta/application/api/PautaRequest.java
+++ src/main/java/com/rlti/actdigital/pauta/application/api/PautaRequest.java
@@ -2,15 +2,16 @@ package com.rlti.actdigital.pauta.application.api;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Data
-public class PautaRequest {
-    @NotBlank(message = "Descrição obrigatória!")
-    private String descricao;
-    private LocalDateTime horarioInicio;
-    @Min(value = 1)
-    Integer tempo;
+public record PautaRequest(
+        @NotBlank(message = "Descrição obrigatória!")
+        String descricao,
+        @NotBlank(message = "Horário obrigatório!")
+        LocalDateTime horarioInicio,
+        @NotBlank(message = "Tempo da Sessão obrigatória!")
+        @Min(value = 1)
+        Integer tempo
+) {
 }
