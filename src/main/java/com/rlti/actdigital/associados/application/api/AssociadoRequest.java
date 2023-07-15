60ebcb1af6ef437e325342dac24e822a47d3d76e
ranelho <ranelhol@gmail.com>
2023-07-15 09:59:26 -0300

    convert class AssociadoRequest em record

--- src/main/java/com/rlti/actdigital/associados/application/api/AssociadoRequest.java
+++ src/main/java/com/rlti/actdigital/associados/application/api/AssociadoRequest.java
@@ -1,18 +1,15 @@
 package com.rlti.actdigital.associados.application.api;
 
-import com.rlti.actdigital.associados.domain.Associado;
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
 import org.hibernate.validator.constraints.br.CPF;
 
-@Data
-public class AssociadoRequest {
-    @NotBlank(message = "Nome obrigatório")
-    private String nome;
-    @NotBlank(message = "CPF obrigatório")
-    @CPF(message = "CPF inválido")
-    @Pattern(regexp = "(^\\d{3}\\x2E\\d{3}\\x2E\\d{3}\\x2D\\d{2}$)")
-    private String cpf;
+public record AssociadoRequest (
+        @NotBlank(message = "Nome obrigatório")
+        String fullName,
+        @NotBlank(message = "CPF obrigatório")
+        @CPF(message = "CPF inválido")
+        @Pattern(regexp = "(^\\d{3}\\x2E\\d{3}\\x2E\\d{3}\\x2D\\d{2}$)")
+        String cpf
+) {
 }
